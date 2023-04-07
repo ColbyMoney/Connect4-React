376b89aaa255c75cce7be7e12449858b8f0bd77c
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2023-04-07 16:52:53 -0500

    Update Gameboard.js
    6x7 grid is displaying properly now, though functionality still needs to be added

--- connect4/src/components/Gameboard.js
+++ connect4/src/components/Gameboard.js
@@ -33,7 +33,9 @@ function Board({ xIsNext, squares, onPlay }) {
   //build the html for the squares in the gameboard so the bottom left corner is (0,0) for (x,y)
   //x corresponds to the column while y corresponds to the row
   //on game board, x value increases to the right and y value increases up
+  /*
   var boardHTML = '';
+  boardHTML += ('<div className="status">{status}</div>')
   for (let y = 5; y >= 0; y--) {
     boardHTML += ('<div className="board-row">');
     for (let x = 0; x < 7; x++) {
@@ -42,21 +44,73 @@ function Board({ xIsNext, squares, onPlay }) {
     boardHTML += ('</div>');
   }
   console.log(boardHTML);
-  //let parser = new DOMParser();
-  //const doc = parser.parseFromString(boardHTML, 'text/html');
+  let parser = new DOMParser();
+  const doc = parser.parseFromString(boardHTML, 'text/html');
+  */
 
-  return (
+  return ( 
     <>
       <div className="status">{status}</div>
-
-      {boardHTML}
-
+      <div className="board-row">
+        <Square value={squares[0][5]} onSquareClick={() => handleClick(0,5)} />
+        <Square value={squares[1][5]} onSquareClick={() => handleClick(1,5)} />
+        <Square value={squares[2][5]} onSquareClick={() => handleClick(2,5)} />
+        <Square value={squares[3][5]} onSquareClick={() => handleClick(3,5)} />
+        <Square value={squares[4][5]} onSquareClick={() => handleClick(4,5)} />
+        <Square value={squares[5][5]} onSquareClick={() => handleClick(5,5)} />
+        <Square value={squares[6][5]} onSquareClick={() => handleClick(6,5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[0][4]} onSquareClick={() => handleClick(0,4)} />
+        <Square value={squares[1][4]} onSquareClick={() => handleClick(1,4)} />
+        <Square value={squares[2][4]} onSquareClick={() => handleClick(2,4)} />
+        <Square value={squares[3][4]} onSquareClick={() => handleClick(3,4)} />
+        <Square value={squares[4][4]} onSquareClick={() => handleClick(4,4)} />
+        <Square value={squares[5][4]} onSquareClick={() => handleClick(5,4)} />
+        <Square value={squares[6][4]} onSquareClick={() => handleClick(6,4)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[0][3]} onSquareClick={() => handleClick(0,3)} />
+        <Square value={squares[1][3]} onSquareClick={() => handleClick(1,3)} />
+        <Square value={squares[2][3]} onSquareClick={() => handleClick(2,3)} />
+        <Square value={squares[3][3]} onSquareClick={() => handleClick(3,3)} />
+        <Square value={squares[4][3]} onSquareClick={() => handleClick(4,3)} />
+        <Square value={squares[5][3]} onSquareClick={() => handleClick(5,3)} />
+        <Square value={squares[6][3]} onSquareClick={() => handleClick(6,3)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[0][2]} onSquareClick={() => handleClick(0,2)} />
+        <Square value={squares[1][2]} onSquareClick={() => handleClick(1,2)} />
+        <Square value={squares[2][2]} onSquareClick={() => handleClick(2,2)} />
+        <Square value={squares[3][2]} onSquareClick={() => handleClick(3,2)} />
+        <Square value={squares[4][2]} onSquareClick={() => handleClick(4,2)} />
+        <Square value={squares[5][2]} onSquareClick={() => handleClick(5,2)} />
+        <Square value={squares[6][2]} onSquareClick={() => handleClick(6,2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[0][1]} onSquareClick={() => handleClick(0,1)} />
+        <Square value={squares[1][1]} onSquareClick={() => handleClick(1,1)} />
+        <Square value={squares[2][1]} onSquareClick={() => handleClick(2,1)} />
+        <Square value={squares[3][1]} onSquareClick={() => handleClick(3,1)} />
+        <Square value={squares[4][1]} onSquareClick={() => handleClick(4,1)} />
+        <Square value={squares[5][1]} onSquareClick={() => handleClick(5,1)} />
+        <Square value={squares[6][1]} onSquareClick={() => handleClick(6,1)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[0][0]} onSquareClick={() => handleClick(0,0)} />
+        <Square value={squares[1][0]} onSquareClick={() => handleClick(1,0)} />
+        <Square value={squares[2][0]} onSquareClick={() => handleClick(2,0)} />
+        <Square value={squares[3][0]} onSquareClick={() => handleClick(3,0)} />
+        <Square value={squares[4][0]} onSquareClick={() => handleClick(4,0)} />
+        <Square value={squares[5][0]} onSquareClick={() => handleClick(5,0)} />
+        <Square value={squares[6][0]} onSquareClick={() => handleClick(6,0)} />
+      </div>
     </>
   );
 }
 
 export default function Gameboard() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([Array(7).fill(Array(6).fill(null))]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
